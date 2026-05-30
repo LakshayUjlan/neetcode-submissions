06589f15dd468c09a71bd92ac915e0dd395f7cba
Lakshay Ujlan <lakshayujlan@gmail.com>
2026-05-30 16:29:59 +0530

    Add: merge-two-binary-trees - submission-0

---
+++ Data Structures & Algorithms/merge-two-binary-trees/submission-0.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
+      def merge(root1,root2):
+        if not root1:
+          return root2
+        if not root2:
+          return root1
+        left=merge(root1.left,root2.left)
+        right=merge(root1.right,root2.right)
+        curr = TreeNode(root1.val + root2.val)
+        curr.left = left
+        curr.right = right
+        return curr
+        
+      return merge(root1,root2)
+
+        
